e59eb9114751ca7bf9b6f88a45612778f871ccd4
OndraNeveril <ondra.neveril@centrum.cz>
2024-08-09 10:50:30 +0200

    Uprava Smajdalfa

--- Smajdalf.py
+++ Smajdalf.py
@@ -2,7 +2,7 @@ from random import randint
 
 #Šmajdalf chlastá místo piva okenu, proměnná pivo tedy bude mít v tomto modulu lokální název okena, globálně se ale jedná stále o pivo
 
-print("------------Načítám balíček Fritol----------------")
+print("------------Načítám balíček Smajdalf----------------")
 
 def Smajdalf1(síla, inteligence, obrana, životy, peníze, okena, tráva, magie, pratele):
     print('Trapnou písničku si zpívám,\nzpívám si trapnou písničku\ntrapnou písničku zpívám\ntu si zpívám já\no jednom malém čaroději, který jede se svým malým koňem na svém malém voze, jede za malým hobitem. A ten malý hobit se ho na něco zeptá... teď!')
@@ -16,4 +16,11 @@ def Smajdalf2(síla, inteligence, obrana, životy, peníze, okena, tráva,magie,
     return síla, inteligence, obrana, životy, peníze, okena, tráva,magie, pratele
 def Smajdalf3(síla, inteligence, obrana, životy, peníze, okena, tráva,magie, pratele):
     print('Já nemám čas!!!')
+    print('KONEC')
+    print('...')
+    print('síla: ', síla)
+    print('inteligence: ', inteligence )
+    print('peníze: ', peníze)
+    print('Zbylo ti ', životy, ' životů')
+    print('Až na pařbu s tebou došli:')
     return síla, inteligence, obrana, životy, peníze, okena, tráva,magie, pratele
